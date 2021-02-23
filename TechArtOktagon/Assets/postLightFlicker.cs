0fd517da9cdcd1d52c23657aadc5a72cf2812ab7
Danny <thedannyvegas@gmail.com>
2021-02-22 23:17:34 -0300

    Sparks particle + Post Light Flickering

---
+++ TechArtOktagon/Assets/postLightFlicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class postLightFlicker : MonoBehaviour
+{
+    private Animator anim;
+    public float minTime;
+    public float maxTime;
+    [Range (0f,1f)]
+    public float offTimeRatio = 0.5f;
+    private float timer;
+    private bool lightOn = true;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("LightOn",lightOn);
+        RestartTimer();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer < 0){
+            lightOn = !lightOn;
+            anim.SetBool("LightOn",lightOn);
+            RestartTimer();
+        }
+    }
+
+    void RestartTimer(){
+        timer = Random.Range(minTime,maxTime);
+        if(!lightOn) timer *= offTimeRatio;
+    }
+}
