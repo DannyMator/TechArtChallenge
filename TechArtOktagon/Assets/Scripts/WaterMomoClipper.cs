d6fc61617b6f916935cfacb16ebc9617cda340f4
Danny <thedannyvegas@gmail.com>
2021-02-25 12:42:29 -0300

    Dust on Momo Step + Material fixes

---
+++ TechArtOktagon/Assets/Scripts/WaterMomoClipper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class WaterMomoClipper : MonoBehaviour
+{
+    private Animator anim;
+
+    public void Start(){
+        anim = GetComponent<Animator>();
+    }
+    public void OnTriggerEnter(Collider other){
+        if(other.tag == "Momo"){
+            anim.SetTrigger("Clip");
+        }
+    }
+}
