0393fdffa83f9e42fdcd509a2117976300fe945c
Danny <thedannyvegas@gmail.com>
2021-02-25 12:42:29 -0300

    Dust on Momo Step + Material fixes + ControllerCanvas setup

---
+++ TechArtOktagon/Assets/Scripts/MomoDustOnStep.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomoDustOnStep : MonoBehaviour
+{
+    public GameObject particle;
+    public void OnTriggerEnter(Collider other){
+        if(other.tag == "Momo"){
+            Vector3 position = other.transform.position + new Vector3 (0f,0.2f,0f);
+            Instantiate(particle, position, Quaternion.Euler(new Vector3(90,0,0)));
+        }
+    }
+}
--- TechArtOktagon/Assets/Scripts/postLightFlicker.cs
+++ TechArtOktagon/Assets/Scripts/postLightFlicker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class postLightFlicker : MonoBehaviour
+public class PostLightFlicker : MonoBehaviour
 {
     private Animator anim;
     public float minTime;
